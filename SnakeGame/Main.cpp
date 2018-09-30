e85a29bbadd7d10a2f58515438452c42a2cf7bb7
huzaifah123 <huzaifah.m123@gmail.com>
2018-10-01 00:28:08 +0100

    Structure of the game
    Skeleton of the game completed.

---
+++ SnakeGame/Main.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWM };
+eDirection dir;
+
+void Setup()
+{
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
+void Draw()
+{
+	system("cls");
+	for (int i = 0; i < width+2; i++)
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0)
+				cout << "#";
+
+			cout << " ";
+
+			if (j == width - 1)
+				cout << "#";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < width + 2; i++)
+		cout << "#";
+	cout << endl;
+
+}
+void Input()
+{
+
+}
+void Logic()
+{
+
+}
+
+int main()
+{
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+		//Sleep(10); sleep(10);
+	}
+
+	return 0;
+}
